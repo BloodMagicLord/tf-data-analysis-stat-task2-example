e587e7e5c74b9450e641f63eb860732aadd77e04
BloodMagicLord <313353@niuitmo.com>
2023-04-07 14:13:03 +0300

    solution added

--- solution.py
+++ solution.py
@@ -1,17 +1,12 @@
-import pandas as pd
+from cmath import sqrt
+
 import numpy as np
 
-from scipy.stats import norm
+chat_id = 632165934  # Ваш chat ID, не меняйте название переменной
+SIGMA = 0.011
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    return np.mean(x) + alpha * sqrt(SIGMA) / sqrt(len(x)), \
+        np.mean(x) + p * sqrt(SIGMA) / sqrt(len(x))
